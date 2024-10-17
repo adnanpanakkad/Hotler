15cdb9830c5878e6c97b3fd1450b1b2826fbb8fe
Adnan <adnancpanakkad@gmail.com>
2024-10-17 16:10:35 +0530

    (update):update the product model

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -4,7 +4,6 @@ class Product {
   final String category;
   final double price;
   final double offerPrice;
-  final String quantity;
   final String shop;
   final String id;
 
@@ -14,7 +13,6 @@ class Product {
     required this.category,
     required this.price,
     required this.offerPrice,
-    required this.quantity,
     required this.shop,
     required this.id,
   });
@@ -27,7 +25,6 @@ class Product {
       category: json['category']['name'] ?? 'Unknown category',
       price: (json['price'] != null) ? double.tryParse(json['price'].toString()) ?? 0.0 : 0.0,
       offerPrice: (json['offerPrice'] != null) ? double.tryParse(json['offerPrice'].toString()) ?? 0.0 : 0.0,
-      quantity: json['quantity'] ?? '0',
       shop: json['shop'] ?? 'Unknown shop',
       id: json['_id'] ?? '',
     );
